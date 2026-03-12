de0d6f7a71175701f3111617cf1a55ad95c4493b
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2026-03-12 19:46:00 +0100

    ms clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { helvetica, instrumentSerif } from "./fonts";
 import "./globals.css";
-
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Beyond | Tvůj Instagram jako byznys",
@@ -15,6 +15,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vuqnag017s");
+          `}
+        </Script>
+      </head>
       <body className={`${helvetica.className} antialiased min-h-screen relative bg-[#111111] overflow-x-hidden`}>
 
         {/* CONTENT LAYER */}
